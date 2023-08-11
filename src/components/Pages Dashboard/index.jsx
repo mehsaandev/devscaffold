5a438628de897a2cfea3ff4e3231e37fbdb28d39
khadija-123 <81877389+khadija-123@users.noreply.github.com>
2023-08-11 15:03:46 +0500

    page dashboard

---
+++ src/components/Pages Dashboard/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Pages Dashboard/navbar.jsx'
+import SideBar from '../Pages Dashboard/sidebar.jsx'
+
+
+const PageDashboard = () => {
+  return (
+    <div className=' dark:bg-slate-700 dark:text-slate-200  '>
+    <Navbar/>
+    <SideBar/>
+    
+    
+    </div>
+    
+  )
+}
+
+export default PageDashboard
