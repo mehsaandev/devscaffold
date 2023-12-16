6b3d0c4e41a9002e26a1d5baa3481a2545d82a5e
Khadija Iqbal <81877389+khadija-123@users.noreply.github.com>
2023-12-16 12:57:43 +0500

    Routing Page

--- src/components/Project Design Dashboard/Routing/index.jsx
+++ src/components/Project Design Dashboard/Routing/index.jsx
@@ -1,122 +1,281 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+  GridRowModes,
+  DataGrid,
+  GridToolbarContainer,
+  GridActionsCellItem,
+  GridRowEditStopReasons,
+} from '@mui/x-data-grid';
+import {
+  randomCreatedDate,
+  randomTraderName,
+  randomId,
+  randomArrayItem,
+} from '@mui/x-data-grid-generator';
 
-const columns = [
-  { id: 'name', label: 'Route Name', minWidth: 170 },
-  { id: 'path', label: 'Route Path', minWidth: 100 },
+
+const roles = ['Active', 'Inactive'];
+const randomRole = () => {
+  return randomArrayItem(roles);
+};
+
+const initialRows = [
   {
-    id: 'page',
-    label: 'Target Page',
-    minWidth: 170,
-    align: 'right',
+    id: randomId(),
+    name: 'Home',
+    path: '/home',
+    page: 'Index.js',
+    crteateDate: randomCreatedDate(),
+    updateDate: randomCreatedDate(),
+    status: randomRole(),
   },
   {
-    id: 'create',
-    label: 'Date Created',
-    minWidth: 170,
-    align: 'right',
-  },
-  {
-    id: 'update',
-    label: 'Date Updated',
-    minWidth: 170,
-    align: 'right',
-    type: 'date',
-  },
-  {
-    id: 'status',
-    label: 'Status',
-    minWidth: 170,
-    align: 'right',
-    type: 'date',
-  },
-  {
-    id: 'action',
-    label: 'Action',
-    minWidth: 170,
-    align: 'right',
+    id: randomId(),
+    name: 'Home',
+    path: '/home',
+    page: 'Index.js',
+    crteateDate: randomCreatedDate(),
+    updateDate: randomCreatedDate(),
+    status: randomRole(),
+  },{
+    id: randomId(),
+    name: 'Home',
+    path: '/home',
+    page: 'Index.js',
+    crteateDate: randomCreatedDate(),
+    updateDate: randomCreatedDate(),
+    status: randomRole(),
+  },{
+    id: randomId(),
+    name: 'Home',
+    path: '/home',
+    page: 'Index.js',
+    crteateDate: randomCreatedDate(),
+    updateDate: randomCreatedDate(),
+    status: randomRole(),
+  },{
+    id: randomId(),
+    name: 'Home',
+    path: '/home',
+    page: 'Index.js',
+    crteateDate: randomCreatedDate(),
+    updateDate: randomCreatedDate(),
+    status: randomRole(),
+  },{
+    id: randomId(),
+    name: 'Home',
+    path: '/home',
+    page: 'Index.js',
+    crteateDate: randomCreatedDate(),
+    updateDate: randomCreatedDate(),
+    status: randomRole(),
   },
 ];
 
-function createData(name, path, page, create, update, status, action) {
-  return { name,path,page,create,update,status,action };
-}
+function EditToolbar(props) {
+  const { setRows, setRowModesModel } = props;
 
-const rows = [
-  createData('Home', '/home', 'index.js', '1/2/2000','1/2/2000',3287263),
-  createData('Home', '/home', 'index.js', '1/2/2000','1/2/2000', 3287263),
-  createData('Home', '/home', 'index.js', '1/2/2000','1/2/2000', 3287263),
-  
-];
-
-export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [...oldRows, { id, name: '', path: '', page: '',isNew: true }]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+    }));
   };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add Route
+      </Button>
+      
+      
+    </GridToolbarContainer>
+  );
+}
+
+export default function FullFeaturedCrudGrid() {
+  const [rows, setRows] = React.useState(initialRows);
+  const [rowModesModel, setRowModesModel] = React.useState({});
+
+  const handleRowEditStop = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns = [
+    { field: 'name', headerName: 'Route Name', width: 180, editable: true },
+    {
+      field: 'path',
+      headerName: 'Route Path',
+      width: 80,
+      align: 'left',
+      headerAlign: 'left',
+      editable: true,
+    },
+    {
+      field: 'page',
+      headerName: 'Target Page',
+      width: 80,
+      align: 'left',
+      headerAlign: 'left',
+      editable: true,
+    },
+    {
+      field: 'createDate',
+      headerName: 'Date Created',
+      type: 'date',
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'updateDate',
+      headerName: 'Date Updated',
+      type: 'date',
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 220,
+      editable: true,
+      type: 'singleSelect',
+      valueOptions: ['Active', 'Inactive'],
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: 'primary.main',
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+  return (
+    <div class="bg-slate-200 w-screen h-screen">
+    <div class="bg-white rounded-lg h-screen mx-8 p-8">
+      <p class="font-sans font-bold text-2xl mb-4"> Project Routes</p>
+      <div class="flex justify-between flex-wrap mb-4 ">
+        <div >
+        <div class="flex rounded-md shadow-sm ring-1  ring-gray-300 sm:max-w-md">
+            <input type="text"  class="block flex-1 border-0 bg-transparent py-1.5 px-1.5 text-gray-900 placeholder:text-gray-400  sm:text-sm sm:leading-6" placeholder="Search"/>
+          </div>
+        </div>
+        <div >
+        <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-1.5 text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">+  Add Routes</a>
+        </div>
+      </div>
+      <Box
+      sx={{
+        height: 500,
+        width: '100%',
+        '& .actions': {
+          color: 'text.secondary',
+        },
+        '& .textPrimary': {
+          color: 'text.primary',
+        },
+      }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        slots={{
+          toolbar: EditToolbar,
+        }}
+        slotProps={{
+          toolbar: { setRows, setRowModesModel },
+        }}
       />
-    </Paper>
+    </Box>
+    </div>
+    </div>
   );
 }
